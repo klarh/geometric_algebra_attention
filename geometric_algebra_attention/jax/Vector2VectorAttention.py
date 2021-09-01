b5db1bbc9700999fa1f85c79e11e68c6e114458f
Matthew Spellings <mspells@vectorinstitute.ai>
2021-09-01 11:31:55 -0400

    Separate out parameters for jax VectorAttention

--- geometric_algebra_attention/jax/Vector2VectorAttention.py
+++ geometric_algebra_attention/jax/Vector2VectorAttention.py
@@ -18,3 +18,25 @@ class Vector2VectorAttention(base.Vector2VectorAttention, VectorAttention):
             self, score_net=score_net, value_net=value_net,
             reduce=reduce, merge_fun=merge_fun, join_fun=join_fun, rank=rank,
             **kwargs)
+
+    def init_fun(self, rng, input_shape):
+        super().init_fun(rng, input_shape)
+
+        rng, next_rng = jax.random.split(rng)
+        _, self.value_net_params = self.value_net_init(next_rng, (self.n_dim,))
+
+        return input_shape, self.params
+
+    @property
+    def params(self):
+        result = super().params
+        result['scale_net_params'] = self.scale_net_params
+        return result
+
+    @property
+    def scale_net(self):
+        return functools.partial(self.scale_net_fn, self.scale_net_params)
+
+    @scale_net.setter
+    def scale_net(self, value):
+        self.scale_net_init, self.scale_net_fn = value
--- geometric_algebra_attention/jax/VectorAttention.py
+++ geometric_algebra_attention/jax/VectorAttention.py
@@ -1,5 +1,7 @@
 
+import collections
 import functools
+import itertools
 import operator
 
 import jax
@@ -30,26 +32,41 @@ class VectorAttention(base.VectorAttention):
 
     @property
     def layer_functions(self):
-        return self.init_fun, type(self).apply_fun
+        return self.init_fun, self.apply_fun
 
     def init_fun(self, rng, input_shape):
         input_shapes = self._parse_inputs(input_shape)
         self.n_dim = input_shapes.values[-1]
 
+        rng, next_rng = jax.random.split(rng)
+        _, self.score_net_params = self.score_net_init(next_rng, (self.n_dim,))
+
+        rng, next_rng = jax.random.split(rng)
+        _, self.value_net_params = self.value_net_init(next_rng, (self.invariant_dims,))
+
+        def rngs(rng):
+            next_rng = rng
+            while True:
+                rng, next_rng = jax.random.split(next_rng)
+                yield rng
+
+        rng_groups, rng_singles = jax.random.split(rng)
+
         weight_sets = self._build_weight_definitions(self.n_dim)
         for (name, defs) in weight_sets.groups.items():
             weights = [
                 rng.normal(stddev=def_.stdev, size=def_.shape)
-                for def_ in defs]
+                for (def_, rng) in zip(defs, rngs(rng))]
             setattr(self, name, weights)
 
-        for (name, def_) in weight_sets.singles.items():
+        for ((name, def_), rng) in zip(weight_sets.singles.items(), rngs(rng_singles)):
             weight = rng.normal(stddev=def_.stdev, size=def_.shape)
             setattr(self, name, weight)
 
-        return input_shape, self
+        return input_shape, self.params
 
-    def apply_fun(self, inputs, rng=None):
+    def apply_fun(self, params, inputs, rng=None):
+        self.params = params
         result = self._evaluate(inputs)
         return result.output
 
@@ -63,3 +80,36 @@ class VectorAttention(base.VectorAttention):
         output = self.math.sum(attention*values, reduce_axes)
 
         return attention, output
+
+    @property
+    def params(self):
+        result = {}
+        weight_sets = self._build_weight_definitions(None)
+        for name in weight_sets.groups:
+            result[name] = getattr(self, name)
+        for name in weight_sets.singles:
+            result[name] = getattr(self, name)
+        result['score_net_params'] = self.score_net_params
+        result['value_net_params'] = self.value_net_params
+        return result
+
+    @params.setter
+    def params(self, values):
+        for (name, val) in values.items():
+            setattr(self, name, val)
+
+    @property
+    def score_net(self):
+        return functools.partial(self.score_net_fn, self.score_net_params)
+
+    @score_net.setter
+    def score_net(self, value):
+        self.score_net_init, self.score_net_fn = value
+
+    @property
+    def value_net(self):
+        return functools.partial(self.value_net_fn, self.value_net_params)
+
+    @value_net.setter
+    def value_net(self, value):
+        self.value_net_init, self.value_net_fn = value
