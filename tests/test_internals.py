ddd8d2e08ab7c709ac2a544844b26c6609e792b3
Matthew Spellings <mspells@vectorinstitute.ai>
2021-10-12 12:24:54 -0400

    Convert unit tests to use multi-scale analysis to identify errors

--- tests/test_internals.py
+++ tests/test_internals.py
@@ -12,6 +12,8 @@ INVARIANT_MODES = ['full', 'partial', 'single']
 
 MERGE_MODES = ['mean', 'concat']
 
+FLOAT_SCALES = np.logspace(2.5, -2.5, 5)
+
 finite_dtype = hnp.from_dtype(
     np.dtype('float32'), min_value=-4, max_value=4,
     allow_nan=False, allow_infinity=False)
@@ -31,6 +33,8 @@ def point_cloud(draw, weights=False):
     N = draw(hs.integers(min_value=4, max_value=12))
     r = draw(hnp.arrays(np.float32, (N, 3), elements=finite_dtype))
     v = draw(hnp.arrays(np.float32, (N, DIM), elements=finite_dtype))
+
+    assume(np.sum(np.square(r)) > 1e-5)
 
     result = [r, v]
 
@@ -58,16 +62,26 @@ class AllTests:
         hs.sampled_from(MERGE_MODES),
         hs.sampled_from(INVARIANT_MODES))
     def test_rotation_invariance_value(self, q, rv, rank, merge_fun, join_fun, invar_mode):
-        r, v = rv
-        rprime = rowan.rotate(q[None], r).astype(np.float32)
+        errs = []
+        for scale in FLOAT_SCALES:
+            r, v = rv; r = r*scale; v = v*scale
+            rprime = rowan.rotate(q[None], r).astype(np.float32)
 
-        key = 'rotation_invariance'
-        prediction1 = self.value_prediction(r, v, key, rank, merge_fun, join_fun, invar_mode)
-        prediction2 = self.value_prediction(rprime, v, key, rank, merge_fun, join_fun, invar_mode)
+            key = 'rotation_invariance'
+            prediction1 = self.value_prediction(r, v, key, rank, merge_fun, join_fun, invar_mode)
+            prediction2 = self.value_prediction(rprime, v, key, rank, merge_fun, join_fun, invar_mode)
 
-        self.assertEqual(v[0].shape, prediction1.shape)
-        delta = max(1e-3, np.mean(np.square(v)))
-        npt.assert_allclose(prediction1 - prediction2, 0, atol=1e-1*delta)
+            self.assertEqual(v[0].shape, prediction1.shape)
+            errs.append(np.mean(np.square(prediction1 - prediction2)))
+
+        with np.errstate(divide='ignore'):
+            x, y = np.log(FLOAT_SCALES), np.log(errs)
+        filt = np.isfinite(y)
+        x, y = x[filt], y[filt]
+        if len(x) > 2:
+            corrcoef = np.corrcoef(x, y)[0, 1]
+            if np.isfinite(corrcoef):
+                self.assertGreater(corrcoef, .9)
 
     @settings(deadline=None)
     @given(
@@ -79,24 +93,33 @@ class AllTests:
         hs.sampled_from(MERGE_MODES),
         hs.sampled_from(INVARIANT_MODES))
     def test_permutation_equivariance_value(self, rv, swap_i, swap_j, rank, merge_fun, join_fun, invar_mode):
-        r, v = rv
-        swap_i = swap_i%len(r)
-        swap_j = swap_j%len(r)
-        rprime, vprime = r.copy(), v.copy()
-        rprime[swap_i], rprime[swap_j] = r[swap_j], r[swap_i]
-        vprime[swap_i], vprime[swap_j] = v[swap_j], v[swap_i]
+        errs = []
+        for scale in FLOAT_SCALES:
+            r, v = rv; r = r*scale; v = v*scale
+            swap_i = swap_i%len(r)
+            swap_j = swap_j%len(r)
+            rprime, vprime = r.copy(), v.copy()
+            rprime[swap_i], rprime[swap_j] = r[swap_j], r[swap_i]
+            vprime[swap_i], vprime[swap_j] = v[swap_j], v[swap_i]
 
-        key = 'permutation_equivariance'
-        prediction1 = self.value_prediction(r, v, key, rank, merge_fun, join_fun, invar_mode, reduce=False)
-        prediction2 = self.value_prediction(rprime, vprime, key, rank, merge_fun, join_fun, invar_mode, reduce=False)
+            key = 'permutation_equivariance'
+            prediction1 = self.value_prediction(r, v, key, rank, merge_fun, join_fun, invar_mode, reduce=False)
+            prediction2 = self.value_prediction(rprime, vprime, key, rank, merge_fun, join_fun, invar_mode, reduce=False)
 
-        self.assertEqual(v.shape, prediction1.shape)
-        temp = prediction2[swap_i].copy()
-        prediction2[swap_i] = prediction2[swap_j]
-        prediction2[swap_j] = temp
+            self.assertEqual(v.shape, prediction1.shape)
+            temp = prediction2[swap_i].copy()
+            prediction2[swap_i] = prediction2[swap_j]
+            prediction2[swap_j] = temp
+            errs.append(np.mean(np.square(prediction1 - prediction2)))
 
-        delta = max(1e-3, np.mean(np.square(v)))
-        npt.assert_allclose(prediction1 - prediction2, 0, atol=1e-1*delta)
+        with np.errstate(divide='ignore'):
+            x, y = np.log(FLOAT_SCALES), np.log(errs)
+        filt = np.isfinite(y)
+        x, y = x[filt], y[filt]
+        if len(x) > 2:
+            corrcoef = np.corrcoef(x, y)[0, 1]
+            if np.isfinite(corrcoef):
+                self.assertGreater(corrcoef, .9)
 
     @settings(deadline=None)
     @given(
@@ -109,32 +132,55 @@ class AllTests:
         hs.sampled_from(INVARIANT_MODES))
     def test_rotation_covariance_vector(self, q, rv, rank, merge_fun, join_fun,
                                         invar_mode, covar_mode):
-        r, v = rv
-        rprime = rowan.rotate(q[None], r).astype(np.float32)
+        errs = []
+        for scale in FLOAT_SCALES:
+            r, v = rv; r = r*scale; v = v*scale
+            rprime = rowan.rotate(q[None], r).astype(np.float32)
 
-        key = 'rotation_covariance'
-        prediction1 = self.vector_prediction(
-            r, v, key, rank, merge_fun, join_fun, invar_mode, covar_mode)
-        prediction1_prime = rowan.rotate(q, prediction1)
-        prediction2 = self.vector_prediction(
-            rprime, v, key, rank, merge_fun, join_fun, invar_mode, covar_mode)
+            key = 'rotation_covariance'
+            prediction1 = self.vector_prediction(
+                r, v, key, rank, merge_fun, join_fun, invar_mode, covar_mode)
+            prediction1_prime = rowan.rotate(q, prediction1)
+            prediction2 = self.vector_prediction(
+                rprime, v, key, rank, merge_fun, join_fun, invar_mode, covar_mode)
 
-        delta = max(1e-3, np.mean(np.square(r)))
-        npt.assert_allclose(prediction1_prime - prediction2, 0, atol=1e-1*delta)
+            errs.append(np.mean(np.square(prediction1_prime - prediction2)))
+
+        with np.errstate(divide='ignore'):
+            x, y = np.log(FLOAT_SCALES), np.log(errs)
+        filt = np.isfinite(y)
+        x, y = x[filt], y[filt]
+        if len(x) > 2:
+            corrcoef = np.corrcoef(x, y)[0, 1]
+            if corrcoef < .9:
+                print(x, y, corrcoef)
+            if np.isfinite(corrcoef):
+                self.assertGreater(corrcoef, .9)
 
     @settings(deadline=None)
     @given(
         unit_quaternions(),
         point_cloud(),
         point_cloud())
-    def test_rotation_covariance_label(self, q, rv, rv2):
-        r, v = rv
-        v2 = rv2[1]
-        rprime = rowan.rotate(q[None], r).astype(np.float32)
+    def test_rotation_covariance_label_vector(self, q, rv, rv2):
+        errs = []
+        for scale in FLOAT_SCALES:
+            r, v = rv; r = r*scale; v = v*scale
+            v2 = rv2[1]
+            rprime = rowan.rotate(q[None], r).astype(np.float32)
 
-        key = 'rotation_covariance_label'
-        prediction1 = self.label_vector_prediction(r, v, v2, key)
-        prediction1_prime = rowan.rotate(q[None], prediction1)
-        prediction2 = self.label_vector_prediction(rprime, v, v2, key)
+            key = 'rotation_covariance_label'
+            prediction1 = self.label_vector_prediction(r, v, v2, key)
+            prediction1_prime = rowan.rotate(q[None], prediction1)
+            prediction2 = self.label_vector_prediction(rprime, v, v2, key)
 
-        npt.assert_allclose(prediction1_prime - prediction2, 0, atol=1e-4)
+            errs.append(np.mean(np.square(prediction1_prime - prediction2)))
+
+        with np.errstate(divide='ignore'):
+            x, y = np.log(FLOAT_SCALES), np.log(errs)
+        filt = np.isfinite(y)
+        x, y = x[filt], y[filt]
+        if len(x) > 2:
+            corrcoef = np.corrcoef(x, y)[0, 1]
+            if np.isfinite(corrcoef):
+                self.assertGreater(corrcoef, .9)
