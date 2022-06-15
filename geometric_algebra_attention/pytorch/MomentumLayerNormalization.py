caf4f44e37ad656c105e71e2352e783782f4127b
Matthew Spellings <mspells@vectorinstitute.ai>
2022-06-15 11:44:31 -0400

    Add MomentumNormalization and MomentumLayerNormalization layers for pytorch

---
+++ geometric_algebra_attention/pytorch/MomentumLayerNormalization.py
@@ -0,0 +1,26 @@
+import torch as pt
+
+from .geometric_algebra import custom_norm
+
+
+class MomentumLayerNormalization(pt.nn.Module):
+    """Exponential decay normalization.
+
+    Calculates a running average of the L2 norm and scales inputs to
+    have length (over the last axis) 1, on average.
+
+    """
+
+    def __init__(self, momentum=0.99, epsilon=1e-7):
+        super().__init__()
+        self.register_buffer('momentum', pt.as_tensor(momentum))
+        self.register_buffer('epsilon', pt.as_tensor(epsilon))
+        self.register_buffer('norm', pt.ones(1))
+
+    def forward(self, x):
+        if self.training:
+            norm = custom_norm(x)
+            norm = pt.mean(norm)
+            self.norm[:] = norm.detach()
+
+        return x/pt.maximum(self.norm, self.epsilon).detach()
---
+++ geometric_algebra_attention/pytorch/MomentumNormalization.py
@@ -0,0 +1,34 @@
+import torch as pt
+
+class MomentumNormalization(pt.nn.Module):
+    """Exponential decay normalization.
+
+    Computes the mean and standard deviation all axes but the last and
+    normalizes values to have mean 0 and variance 1; suitable for
+    normalizing a vector of real-valued quantities with differing
+    units.
+
+    """
+
+    def __init__(self, n_dim, momentum=.99):
+        super().__init__()
+        self.n_dim = n_dim
+        self.register_buffer('momentum', pt.as_tensor(momentum))
+        self.register_buffer('mu', pt.zeros(n_dim))
+        self.register_buffer('sigma', pt.ones(n_dim))
+
+    def forward(self, x):
+        if self.training:
+            axes = tuple(range(x.ndim - 1))
+            mu_calc = pt.mean(x, axes, keepdim=False)
+            sigma_calc = pt.std(x, axes, keepdim=False, unbiased=False)
+
+            new_mu = self.momentum*self.mu + (1 - self.momentum)*mu_calc
+            new_sigma = self.momentum*self.sigma + (1 - self.momentum)*sigma_calc
+
+            self.mu[:] = new_mu.detach()
+            self.sigma[:] = new_sigma.detach()
+
+        sigma = pt.maximum(self.sigma, pt.as_tensor(1e-7))
+
+        return (x - self.mu.detach())/sigma.detach()
